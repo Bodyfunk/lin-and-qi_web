c40f526b8b784a9c3ded6b031d4e76d6bcc2a297
Bodyfunk <616982274@qq.com>
2022-07-06 00:01:02 +0800

    M:home页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,129 @@
 <template>
-    
+    <div class="home">
+        <div class="content">
+            <!-- 主页头 -->
+            <div class="home-header">
+                <router-link
+                class="link"
+                to="/"
+                >
+                <img
+                    class="home-logo"
+                    src="../assets/image/ku.png"
+                    alt="aaayi logo"
+                />
+                </router-link>
+            </div>
+        <!-- 主页 内容 -->
+            <div class="home-body">
+                <div class="list">
+                <router-link
+                    class="link"
+                    to="/articles"
+                >
+                    文章
+                </router-link>
+                <router-link
+                    class="link"
+                    to="/project"
+                >
+                    项目
+                </router-link>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link"
+                    href="https://github.com/biaochenxuying"
+                >
+                    github
+                </a>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link"
+                    href="https://juejin.im/user/591d6b4d0ce463006926ae40"
+                >
+                    掘金
+                </a>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link"
+                    href="https://www.zhihu.com/people/mo-xu-ming-53/posts"
+                >
+                    知乎
+                </a>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link"
+                    href="https://segmentfault.com/u/biaochenxuying"
+                >
+                    segmentfault
+                </a>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link"
+                    href="https://www.jianshu.com/u/91717b553bfd"
+                >
+                    简书
+                </a>
+                </div>
+                <div class="introduce"> 时光正好，未来可期，加油 ！ </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 console.log(import.meta.env.VITE_APP_TITLE)
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.home {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('../assets/image/bro.png');
+  .content {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    font-size: 20px;
+    text-align: center;
+    padding-top: 12%;
+    .home-logo {
+      width: 220px;
+      border-radius: 50%;
+    }
+    .home-header {
+      .link {
+        display: block;
+      }
+    }
+    .home-body {
+      padding-top: 20px;
+      .list {
+        .link {
+          display: inline-block;
+          padding: 20px;
+          color: #409eff;
+          min-width: 80px;
+        }
+        .link:hover {
+          color: rgb(155, 35, 35);
+        }
+      }
+    }
+    .introduce {
+      padding-top: 20px;
+      color: #fff;
+    }
+  }
+}
 </style>
