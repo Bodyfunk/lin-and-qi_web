d1459f922763c1ce85090791cae43b47a1387396
Bodyfunk <616982274@qq.com>
2022-07-26 20:06:54 +0800

    M:临时提交

--- src/views/ArticleContent/index.vue
+++ src/views/ArticleContent/index.vue
@@ -2,10 +2,14 @@
   <div class="layout">
     <div class="left">
       <div class="fixed-catalogue">
-        <strong style="font-size: 18px;">导航目录:</strong>
+        <strong style="font-size: 18px">导航目录:</strong>
         <ul class="catalogue-box">
           <li v-for="item in catalogueList" :key="item.title">
-            <a :href="'#'+item.id"><span :style="'margin-left:'+(4*item.level)+'px;'">{{item.title}}</span> </a>
+            <a :href="'#' + item.id"
+              ><span :style="'margin-left:' + 4 * item.level + 'px;'">{{
+                item.title
+              }}</span>
+            </a>
           </li>
         </ul>
       </div>
@@ -13,84 +17,91 @@
     <div class="middle">
       <div class="info-main">
         <div v-if="loading">loading</div>
-        <div v-else v-html="articleContent" class="entry-content" id="previewer"></div>
+        <div
+          v-else
+          v-html="articleContent"
+          class="entry-content"
+          id="previewer"
+        ></div>
       </div>
     </div>
-    <div class="right"></div>
+    <div class="right">
+      <div class="user-container">用户信息</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { nextTick, onMounted, ref, watch } from 'vue-demi'
-import { useRoute } from 'vue-router'
-import { getArticleInfo } from '../../api/article'
+import { nextTick, onMounted, ref, watch } from "vue-demi";
+import { useRoute } from "vue-router";
+import { getArticleInfo } from "../../api/article";
 // import Catalogue from './component/catalogue.vue'
 
 interface CatalogueItem {
-  id: string
-  key: string
-  title: string
-  level: number
+  id: string;
+  key: string;
+  title: string;
+  level: number;
 }
 
-const loading = ref(true)
-const articleContent = ref({})
-const route = useRoute()
-const query = route.query
+const loading = ref(true);
+const articleContent = ref({});
+const route = useRoute();
+const query = route.query;
 
 const getInfo = () => {
-  getArticleInfo(query.cate, query.title).then((res) => {
-    loading.value = false
-    articleContent.value = res
-  })
-}
-getInfo()
+  getArticleInfo(query.cate, query.title).then((res: string) => {
+    loading.value = false;
+    articleContent.value = res;
+  });
+};
+getInfo();
 
-const catalogueList = ref<CatalogueItem[]>()
+const catalogueList = ref<CatalogueItem[]>();
 const getArticleNode = () => {
   //拿到文章内容的节点
-  let parent = document.getElementById('previewer')
+  let parent = document.getElementById("previewer");
   //拿到文章里面的h1-h6标签
   let doms: NodeListOf<Element> | undefined =
-    parent?.querySelectorAll('h1,h2,h3,h4,h5,h6')
-  let hEles = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    parent?.querySelectorAll("h1,h2,h3,h4,h5,h6");
+  let hEles = ["h1", "h2", "h3", "h4", "h5", "h6"];
   //结果集
-  let catalogue: CatalogueItem[] = []
-  let index = 0
+  let catalogue: CatalogueItem[] = [];
+  let index = 0;
   if (doms && doms.length > 0) {
     doms?.forEach((el) => {
-      let nodeText = el.innerHTML.replace(/<\/?[^>]+>/g, '')
-      nodeText = nodeText.replace(/&nbsp;/gi, '').replace(/:|：/, '')
-      let name = el.nodeName.toLowerCase()
+      let nodeText = el.innerHTML.replace(/<\/?[^>]+>/g, "");
+      nodeText = nodeText.replace(/&nbsp;/gi, "").replace(/:|：/, "");
+      let name = el.nodeName.toLowerCase();
       if (hEles.includes(name)) {
-        index++
-        let id = `catalogue_${index}`
+        index++;
+        let id = `catalogue_${index}`;
         // 为当前节点添加id
-        el.setAttribute('id', id)
-        let level = name.replace('h', '')
+        el.setAttribute("id", id);
+        let level = name.replace("h", "");
         catalogue.push({
           id: id,
           key: name,
           title: nodeText,
           level: Number(level),
-        })
+        });
       }
-    })
+    });
   }
-  return catalogue
-}
+  return catalogue;
+};
 
 onMounted(() => {
-  if (document.getElementById('previewer')) {
-    catalogueList.value = getArticleNode()
+  if (document.getElementById("previewer")) {
+    catalogueList.value = getArticleNode();
   }
-})
+});
 
 watch(articleContent, () => {
   nextTick(() => {
-    catalogueList.value = getArticleNode()
-  })
-})
+    catalogueList.value = getArticleNode();
+  });
+});
 </script>
 
 <style scoped lang="scss">
